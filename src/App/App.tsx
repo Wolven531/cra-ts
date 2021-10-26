79020bba62ee9fe947cbbaa7e4dba7608e742978
wolven531 <anthony.williams.cs@gmail.com>
2021-10-25 19:07:03 -0600

    Move App

---
+++ src/App/App.tsx
@@ -0,0 +1,24 @@
+import React, { FC, useState } from 'react'
+import { Tabs } from '../Tabs'
+import './App.css'
+
+export const App: FC<any> = () => {
+	const [selectedTab, setSelectedTab] = useState(1)
+
+	// useEffect(() => {}, [])
+
+	return (
+		<article className="app">
+			<header className="">
+				<h3>TS Playground</h3>
+			</header>
+			<Tabs onTabChange={setSelectedTab} selectedTab={selectedTab} />
+			<section className="content">
+				{selectedTab === 1 && <div>Content 1</div>}
+				{selectedTab === 2 && <div>Content 2</div>}
+				{selectedTab === 3 && <div>Content 3</div>}
+			</section>
+			<footer>&copy; 2021 Anthony Williams</footer>
+		</article>
+	)
+}
