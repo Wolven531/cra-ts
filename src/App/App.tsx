631edd338a882bad35d4df07176326cd691705d7
wolven531 <anthony.williams.cs@gmail.com>
2021-10-27 23:16:50 -0600

    Use lifted state from Tabs

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useCallback, useEffect, useState } from 'react'
+import { useHistory, useParams } from 'react-router-dom'
 import { Tabs } from '../Tabs'
 import './App.css'
 
@@ -7,18 +8,40 @@ export interface AppProps {
 }
 
 export const App: FC<AppProps> = ({ initialTab }) => {
+	const history = useHistory()
+	const params = useParams<{ tabId?: string }>()
+
 	const [selectedTab, setSelectedTab] = useState(
 		initialTab !== undefined ? initialTab : 1
 	)
 
-	// useEffect(() => {}, [])
+	const handleTabChange = useCallback(
+		(tabNum: number) => {
+			setSelectedTab(tabNum)
+			history.push(`/tabs/${tabNum}`)
+		},
+		[history, setSelectedTab]
+	)
+
+	/**
+	 * Runs on component mount
+	 */
+	useEffect(() => {
+		if (params.tabId === undefined) {
+			handleTabChange(selectedTab)
+			return
+		}
+
+		const paramTab = parseInt(params.tabId)
+		handleTabChange(paramTab)
+	}, [handleTabChange, params.tabId, selectedTab])
 
 	return (
 		<article className="app" data-testid="app">
 			<header>
 				<h3>TS Playground</h3>
 			</header>
-			<Tabs onTabChange={setSelectedTab} selectedTab={selectedTab} />
+			<Tabs onTabChange={handleTabChange} selectedTab={selectedTab} />
 			<section className="content" data-testid="content">
 				{selectedTab === 1 && <div>Content 1</div>}
 				{selectedTab === 2 && <div>Content 2</div>}
